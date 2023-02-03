511fc79650af7fba0db976b411ef5fc7e4bcc125
TomBenRu <mail@thomas-ruff.de>
2023-02-03 18:52:32 +0100

    still work in progress Versuch, remainder mit apscheduler zu integrieren.

--- utilities/scheduler.py
+++ utilities/scheduler.py
@@ -1,4 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
-scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 20,
-                                 'apscheduler.job_defaults.misfire_grace_time': 600})
+scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 2,
+                                 'apscheduler.job_defaults.misfire_grace_time': 1200})
+scheduler.configure()
