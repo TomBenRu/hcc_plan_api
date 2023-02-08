d2cb9b635e80b7ff583cbc13958940270373226b
TomBenRu <mail@thomas-ruff.de>
2023-02-08 18:44:07 +0100

    still work in progress username to database

--- databases/database.py
+++ databases/database.py
@@ -11,7 +11,7 @@ from .enum_converter import EnumConverter
 
 # zum Deployen müssen server_remote_access, local und from_outside False sein
 server_remote_acces = False
-local = True  # True: sqlite-database, False: postgresql-database
+local = False  # True: sqlite-database, False: postgresql-database
 from_outside = False  # False: calling database from same API
 
 # sql_debug(True)
